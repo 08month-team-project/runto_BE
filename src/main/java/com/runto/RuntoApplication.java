948fd0aec1c6796e755ab77e2a708536acce219b
DokyungYou <willow74rr@gmail.com>
2024-10-23 14:41:10 +0900

    프로젝트 초기화

---
+++ src/main/java/com/runto/RuntoApplication.java
@@ -0,0 +1,13 @@
+package com.runto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuntoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuntoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/runto/RuntoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuntoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
