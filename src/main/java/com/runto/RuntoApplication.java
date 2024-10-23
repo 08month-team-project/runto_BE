6f8d119076ef910c24bb24f9ea3ad794666109b5
DokyungYou <willow74rr@gmail.com>
2024-10-23 22:09:46 +0900

    ✨Feat: RuntoApplication 에 세팅 때 누락된 @ EnableJpaAuditing 추가

--- src/main/java/com/runto/RuntoApplication.java
+++ src/main/java/com/runto/RuntoApplication.java
@@ -2,7 +2,9 @@ package com.runto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class RuntoApplication {
 
